9b4f9c0264fe9f410951cfdc841d5128a6b028bc
ignatius-one <josegnacio118@gmail.com>
2021-07-24 21:09:15 -0500

    e

--- js/scrip.js
+++ js/scrip.js
@@ -1,4 +1,13 @@
+
 const menu = document.getElementById('btn-open-menu')
 menu.addEventListener('click',()=> {
     document.getElementById('container-menu').classList.toggle('active')
 })
+
+$(function () {
+    let header = document.getElementById('container-menu');
+    let headroom = new Headroom(header);
+    headroom.init();   
+})
+
+document.getElementById('date').innerHTML = new Date().getFullYear()
