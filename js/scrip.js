596dcc3a3b6daf6836b44bc33f0be9ef6847c5f7
ignatius-one <josegnacio118@gmail.com>
2021-08-02 20:09:38 -0500

    a

--- js/scrip.js
+++ js/scrip.js
@@ -1,13 +1,18 @@
 
-const menu = document.getElementById('btn-open-menu')
-menu.addEventListener('click',()=> {
-    document.getElementById('container-menu').classList.toggle('active')
-})
+const btn = document.querySelector('#btn-on')
+const menu = document.querySelector('.open')
 
+btn.addEventListener('click', () => {
+    menu.classList.toggle('active')
+
+      if (menu.classList.contains('active')) {
+        btn.setAttribute('aria-label','close-menu')
+    } else {
+        btn.setAttribute('aria-label','open-menu')
+    }
+})
 $(function () {
     let header = document.getElementById('container-menu');
     let headroom = new Headroom(header);
     headroom.init();   
 })
-
-document.getElementById('date').innerHTML = ` ${new Date().getFullYear()}`
